866e311c1ab25a9871fe5e2d4163e33843b05788
Kevin Wong <kevinsolomonwong@gmail.com>
2023-11-14 09:50:10 -0800

    started bots

---
+++ parental_bot.py
@@ -0,0 +1,4 @@
+# Parental bot
+# Author: Kevin Wong
+# November 14 2023
+
--- similar_hobbies.py
+++ similar_hobbies.py
@@ -1,1 +1,7 @@
 # Similar hobbies bot
+# Author: Kevin Wong
+# November 14 2023
+
+# Greet User
+print("Hellow my friend I will calculate your similarity score between  you and another person.")
+
