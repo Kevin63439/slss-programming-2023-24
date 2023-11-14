b01c6925fb9b22f02a0ed3cdfa95ec8e5f54b7b6
Kevin Wong <kevinsolomonwong@gmail.com>
2023-11-14 09:16:31 -0800

    Changed bot

--- sfus_best_sim_score.py
+++ sfus_best_sim_score.py
@@ -22,36 +22,36 @@ profile = [
 ]
 
 # Initialize our top similarity score and their name.
-top_sim_score = 0
+top_sim_score = 5
 top_sim_name = ""
 
 with open("./data.csv") as f:
     # Throw away the header line
     header = f.readline()
 
-# For every line of data in the file (string):
-for line in f:
-    # Convert the line of data into a list
-    current_likes = line.split(",")
+    # For every line of data in the file (string):
+    for line in f:
+        # Convert the line of data into a list
+        current_likes = line.split(",")
 
-    # Initialize the CURRENT similarity score
-    # Store the current person's name
-    current_sim_score = 0
-    current_name = current_likes[1]
+        # Initialize the CURRENT similarity score
+        # Store the current person's name
+        current_sim_score = 0
+        current_name = current_likes[1]
 
-    # sim score algo
-    for item in profile:
-        if item in current_likes:
-            current_sim_score += 1
-    
-    # print the current sim_score
-    print(f"{current_sim_score} - Score: {current_sim_score}")
+        # sim score algo
+        for item in profile:
+            if item in current_likes:
+                current_sim_score += 1
+        
+        # print the current sim_score
+        print(f"{current_name} - Score: {current_sim_score}")
 
-    # If the cur score is > top sim score
-if current_sim_score > top_sim_score:
-    # update the top sim score and name
-    top_sim_score = current_sim_score
-    top_sim_score = current_name
+            # If the cur score is < top sim score
+        if current_sim_score < top_sim_score:
+            # update the top sim score and name
+            top_sim_score = current_sim_score
+            top_sim_name = current_name
 
-print("TOP SIMILAR PERSON!")
+print("LEAST SIMILAR PERSON!")
 print(f"{top_sim_name} - Score: {top_sim_score}")
