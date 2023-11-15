cd62f9b884f688d4271fd102af24d4a7d81bd6de
Kevin Wong <kevinsolomonwong@gmail.com>
2023-11-15 11:15:49 -0800

    parental bot done

--- parental_bot.py
+++ parental_bot.py
@@ -2,3 +2,31 @@
 # Author: Kevin Wong
 # November 14 2023
 
+# Greet the user
+print("Hi there I'm a parental bot that will see how good you are.")
+
+# Make list with questions
+questions = [
+    "Did you eat?",
+    "Did you study?",
+    "Did you do your laundry?",
+    "Did you call grandma?"
+
+]
+
+# Initialize score
+number_yes = 0
+
+# Get scores
+for question in questions:
+    if input(question).strip(",.?!").lower()=="yes":
+        number_yes += 1
+
+# Print response based on number of yes
+if number_yes == 0:
+    print("I'm coming over!!")
+elif 1 > number_yes > 2:
+    print("Ok") 
+else:
+    print("Good!")
+
