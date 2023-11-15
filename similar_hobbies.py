2031db8ed9b5a7c2ec302f4356ee36053d3d031a
Kevin Wong <kevinsolomonwong@gmail.com>
2023-11-15 10:44:59 -0800

    finished hobbies bot

--- similar_hobbies.py
+++ similar_hobbies.py
@@ -3,5 +3,19 @@
 # November 14 2023
 
 # Greet User
-print("Hellow my friend I will calculate your similarity score between  you and another person.")
+print("Hello my friend I will calculate your similarity score between you and another person.")
 
+person1_hobbies = input("So user 1 please enter 3 hobbies, separated by spaces.").lower().split()
+
+person2_hobbies = input("So user 2 please enter 3 hobbies, separated by spaces.").lower().split()
+
+# Initialize Similarity Score
+similarity_score = 0
+
+# Iterate through all movies in first list
+for hobbies in person1_hobbies: 
+    if hobbies in person2_hobbies:
+        similarity_score += 1
+
+# Display results
+print(f"User 1 and User 2 have {similarity_score} hobbies in common!")
