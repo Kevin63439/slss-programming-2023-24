b45ca6412b705010aee5dfb7e9245b9eae2bd847
Kevin Wong <kevinsolomonwong@gmail.com>
2023-12-13 11:57:34 -0800

    did stuff

---
+++ colour_helper.py
@@ -0,0 +1,18 @@
+# Colour helper
+# Author: Kevin Wong
+# December 13 2023
+
+def pixel_to_string(pixel: tuple) -> str:
+    """Take a rgb 3-tuple and "interpret it"
+    as a colour and return that colour's name
+
+    Params:
+        pixel - 3-tuple of (red, green, blue)
+
+    Return:
+        String representing the colour
+    """
+    r, g, b = pixel
+
+    if g > 250 and r < 32 and b < 32:
+        return "green"
